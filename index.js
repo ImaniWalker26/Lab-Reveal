13bb0b319e5198d2c7b9560263415c4a540f2bac
Imani Walker <walkerim@msu.edu>
2025-02-26 23:30:19 +0000

    Final

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Add JavaScript below
+ScrollReveal().reveal('.SR1', {
+    distance: '60px',
+    origin: 'bottom',
+    duration: 1000,
+    delay: 100,
+    opacity: 0,
+    reset: true
+});
+
+ScrollReveal().reveal('.SR2', {
+    distance: '150px',
+    origin: 'left',
+    duration: 1600,
+    delay: 600,
+    opacity: 0,
+    reset: true
+});
+
+ScrollReveal().reveal('.SR3', {
+    distance: '100px',
+    origin: 'right',
+    duration: 1250,
+    delay: 200,
+    opacity: 0,
+    reset: true
+});
+
+ScrollReveal().reveal('.SR4', {
+    distance: '120px',
+    origin: 'top',
+    duration: 800,
+    delay: 800,
+    opacity: 0,
+    reset: true
+});
+
+ScrollReveal().reveal('.SR5', {
+    distance: '180px',
+    origin: 'bottom',
+    duration: 1360,
+    delay: 1000,
+    opacity: 0,
+    reset: true
+});
+
+
